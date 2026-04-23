e55174cdb543658e1af2f70890a8b894520b4d87
Daniel Mazzeu Dev <223612668+danielmazzeudev@users.noreply.github.com>
2026-04-23 14:10:45 -0300

    Add Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -10,6 +11,15 @@ async function bootstrap() {
     credentials: true,
   });
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Clientes Farmacia API')
+    .setDescription('Documentacao da API de clientes, produtos e vendas')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
